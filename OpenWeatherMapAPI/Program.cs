4bd1da992a8347174418caad8d139f06aa1996b4
Katelyn Yaeger <knyaeger93@gmail.com>
2023-10-19 16:05:04 -0500

    try-catch added

--- OpenWeatherMapAPI/Program.cs
+++ OpenWeatherMapAPI/Program.cs
@@ -21,30 +21,39 @@ var _apiKey = apiSettings.Value.ApiKey;
 
 var client = new HttpClient();
 
+
 while (true)
 {
-    Console.WriteLine();
-    Console.WriteLine("Please enter your city name:");
-
-    var cityName = Console.ReadLine();
-
-    var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={_apiKey}";
-
-    var weatherResponse = client.GetStringAsync(weatherURL).Result;
-
-    var formattedResponse = JObject.Parse(weatherResponse).GetValue("main").ToString();
-
-    Console.WriteLine("----");
-
-    Console.WriteLine($"{formattedResponse}");
-
-    Console.WriteLine("Would you like to choose a different city? yes or no");
-    var userAnswer = Console.ReadLine();
-
-    if (userAnswer.ToLower() == "no")
+    try
     {
-        Console.WriteLine("Goodbye then!");
-        break;
+        Console.WriteLine();
+        Console.WriteLine("Please enter your city name:");
+
+        var cityName = Console.ReadLine();
+
+        var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={_apiKey}";
+
+        var weatherResponse = client.GetStringAsync(weatherURL).Result;
+
+        var formattedResponse = JObject.Parse(weatherResponse).GetValue("main").ToString();
+
+        Console.WriteLine("----");
+
+        Console.WriteLine($"{formattedResponse}");
+
+        Console.WriteLine("Would you like to choose a different city? yes or no");
+        var userAnswer = Console.ReadLine();
+
+        if (userAnswer.ToLower() == "no")
+        {
+            Console.WriteLine("Goodbye then!");
+            break;
+        }
+    }
+
+    catch (Exception e)
+    {
+        Console.WriteLine($"Hmm, that doesn't look right. Please try again.");
     }
 
 }
